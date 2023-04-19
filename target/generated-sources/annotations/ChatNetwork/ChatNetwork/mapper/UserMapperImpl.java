814c490a99013241864953e87b39cf3123446e9c
Phatchara_Mix <99187452+MiracleX77@users.noreply.github.com>
2023-04-20 00:32:34 +0700

    .

--- target/generated-sources/annotations/ChatNetwork/ChatNetwork/mapper/UserMapperImpl.java
+++ target/generated-sources/annotations/ChatNetwork/ChatNetwork/mapper/UserMapperImpl.java
@@ -2,15 +2,14 @@ package ChatNetwork.ChatNetwork.mapper;
 
 import ChatNetwork.ChatNetwork.entity.User;
 import ChatNetwork.ChatNetwork.model.MRegisterResponse;
+import javax.annotation.processing.Generated;
 import org.springframework.stereotype.Component;
 
-/*
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-04-18T20:50:48+0700",
-    comments = "version: 1.5.3.Final, compiler: Eclipse JDT (IDE) 3.33.0.v20230218-1114, environment: Java 17.0.6 (Eclipse Adoptium)"
+    date = "2023-04-19T23:53:01+0700",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 20 (Oracle Corporation)"
 )
-*/
 @Component
 public class UserMapperImpl implements UserMapper {
 
@@ -21,9 +20,6 @@ public class UserMapperImpl implements UserMapper {
         }
 
         MRegisterResponse mRegisterResponse = new MRegisterResponse();
-
-        mRegisterResponse.setEmail( user.getEmail() );
-        mRegisterResponse.setName( user.getName() );
 
         return mRegisterResponse;
     }
